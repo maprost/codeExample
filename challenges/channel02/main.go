4602d0437e9c4a68ca4c32c6e57129b11c2d6a59
Matthias <matthias.leuthaeuser@gmx.de>
2025-03-13 09:31:29 +0100

    add some challenges

--- challenges/channel02/main.go
+++ challenges/channel02/main.go
@@ -9,7 +9,7 @@ func main() {
 	ch := make(chan *int, 4)
 	array := []int{1, 2, 3, 4}
 	wg := sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(len(array))
 	go func() {
 		for _, value := range array {
 			ch <- &value
@@ -18,17 +18,17 @@ func main() {
 	go func() {
 		for value := range ch {
 			fmt.Println(*value)
+			wg.Done()
 		}
-		wg.Done()
 	}()
 
 	// New goroutine is run.
-	go func() {
-		var a int
-		for { // will run forever -> programm can not stop
-			a++
-		}
-	}()
+	//go func() {
+	//	var a int
+	//	for { // will run forever -> programm can not stop
+	//		a++
+	//	}
+	//}()
 
 	wg.Wait()
 }
--- challenges/channel03/main.go
+++ challenges/channel03/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	ch := make(chan *int, 4)
+	array := []int{1, 2, 3, 4}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for _, value := range array {
+			ch <- &value
+		}
+		close(ch)
+	}()
+	go func() {
+		for value := range ch {
+			fmt.Println(*value)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
